c9c35b8f53e458e10d1d8b8994e8a3adedbe67ec
PolAleks <aa.polushkin@yandex.ru>
2026-06-17 19:15:09 +0300

    внедрил connection в QuestionsSqlRepository

--- src/DevQuestions.Infrastructure.Postgres/Repositories/QuestionsSqlRepository.cs
+++ src/DevQuestions.Infrastructure.Postgres/Repositories/QuestionsSqlRepository.cs
@@ -1,3 +1,5 @@
+using Dapper;
+using DevQuestions.Application.Database;
 using DevQuestions.Application.Questions;
 using DevQuestions.Domain.Question;
 
@@ -5,11 +7,34 @@ namespace DevQuestions.Infrastructure.Postgres.Repositories;
 
 public class QuestionsSqlRepository : IQuestionsRepository
 {
+    private readonly ISqlConnection _connectionFactory;
+
+    public QuestionsSqlRepository(ISqlConnection connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
     public async Task<Guid> AddAsync(Question question, CancellationToken cancellationToken)
     {
+        const string sql = """
+                           INSERT INTO Questions (id, title, text, user_id, screenshot_id, tags, status)
+                           VALUES (@Id, @Title, @Text, @UserId, @ScreenshotId, @Tags, @Status);
+                           """;
 
+        using var connection = _connectionFactory.GetConnection();
 
-        throw new NotImplementedException();
+        await connection.ExecuteAsync(sql, new
+        {
+            Id = question.Id,
+            Title = question.Title,
+            Text = question.Text,
+            UserId = question.UserId,
+            ScreenshotId = question.ScreenshotId,
+            Tags = question.Tags.ToArray(),
+            Status = question.Status
+        });
+        
+        return question.Id;
     }
 
     public async Task<Guid> DeleteAsync(Guid questionId, CancellationToken cancellationToken)
