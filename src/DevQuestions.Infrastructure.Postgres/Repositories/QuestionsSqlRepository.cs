deee4ac645199fac95fe457857c87a0aa26d1950
PolAleks <aa.polushkin@yandex.ru>
2026-06-17 17:59:22 +0300

    создал заготовку QuestionsSqlRepository.cs для работы с бд через dapper

---
+++ src/DevQuestions.Infrastructure.Postgres/Repositories/QuestionsSqlRepository.cs
@@ -0,0 +1,34 @@
+using DevQuestions.Application.Questions;
+using DevQuestions.Domain.Question;
+
+namespace DevQuestions.Infrastructure.Postgres.Repositories;
+
+public class QuestionsSqlRepository : IQuestionsRepository
+{
+    public async Task<Guid> AddAsync(Question question, CancellationToken cancellationToken)
+    {
+
+
+        throw new NotImplementedException();
+    }
+
+    public async Task<Guid> DeleteAsync(Guid questionId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Question?> GetByIdAsync(Guid questionId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<int> GetOpenUserQuestionsAsync(Guid userId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SaveAsync(Question question, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
