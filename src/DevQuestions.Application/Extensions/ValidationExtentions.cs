5ffa63aa053f58efa6f6c5627555ffc362037233
PolAleks <aa.polushkin@yandex.ru>
2026-06-19 14:19:39 +0300

    изменил возращаемы тип метода расширения с Error[] на обертку Failure

--- src/DevQuestions.Application/Extensions/ValidationExtentions.cs
+++ src/DevQuestions.Application/Extensions/ValidationExtentions.cs
@@ -5,9 +5,9 @@ namespace DevQuestions.Application.Extensions;
 
 public static class ValidationExtentions
 {
-    public static Error[] ToErrors(this ValidationResult validationResult)
+    public static Failure ToErrors(this ValidationResult validationResult)
     {
         return validationResult.Errors.Select(e => Error.Validation(
-                e.ErrorCode, e.ErrorMessage, e.PropertyName)).ToArray();
+                e.ErrorCode, e.ErrorMessage, e.PropertyName)).ToList();
     }
 }
